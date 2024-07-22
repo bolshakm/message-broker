b73c167f8dbe39119bc655780ca9d6f1b7405cc0
maksym_bolshak <bolshakmv@gmail.com>
2024-07-22 13:27:17 +0200

    add jar file

--- src/main/java/com/tenet/messagebroker/controller/MessageController.java
+++ src/main/java/com/tenet/messagebroker/controller/MessageController.java
@@ -38,7 +38,7 @@ public class MessageController {
             return ResponseEntity.badRequest().build();
         }
 
-        LOG.info("Message from {}, text: {}", request.getLogin(), request.getText());
+        LOG.info("Message from {}, topic {}, text: {}", request.getLogin(), request.getTopicName(), request.getText());
         return ResponseEntity.ok(messageService.post(request));
 
     }
